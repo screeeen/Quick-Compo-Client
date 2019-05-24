19b9d88d546650073f020684c2a51e4af1b89044
screeeen <miguel@screenimplosion.com>
2019-05-24 18:15:41 +0200

    housekeeping

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import { withRouter } from 'react-router'
 import { withAuth } from "../lib/AuthProvider";
 import './Navbar.css';
